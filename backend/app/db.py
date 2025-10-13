226641af0a5b5fccab98960074c15a81ceeb7b1d
danielFrancafatecspLabs <daniel.leite7@fatec.sp.gov.br>
2025-10-13 16:34:05 -0300

    fix: remove fallback MongoDB database environment variable

--- backend/app/db.py
+++ backend/app/db.py
@@ -6,7 +6,6 @@ import time
 
 load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://danielfrancaleite:consolidacaofornecedor@cluster0.ek2c8kp.mongodb.net/")
-MONGODB_DB = os.getenv("MONGODB_DB", "fornecedores_db")
 
 ## Não usar fallback local, sempre usar Atlas
 
